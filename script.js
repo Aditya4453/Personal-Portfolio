9447c60a68bf5a172e074a68e9051c96e5f51aee
Aditya <modaniaditya06@gmail.com>
2025-04-20 22:37:20 +0530

    datetime

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const navbar = document.getElementById("header");
-window.addEventListener("scroll", () => {
-    if (window.scrollY === 0) {
-        navbar.style.backgroundColor='rgba(0, 0, 0, 0)';
-    } else {
-        navbar.style.backgroundColor='rgba(0, 0, 0, 0.817)';
-    }
-});
+function showDatetime() {
+    const now = new Date();
+    const options = {
+        weekday: 'long',   // e.g., "Sunday"
+        year: 'numeric',   // e.g., "2025"
+        month: 'long',     // e.g., "April"
+        day: 'numeric'     // e.g., "20"
+      };
+      
+    const date = now.toLocaleDateString('en-IN', options);
+    const time = now.toLocaleTimeString();
+
+    document.getElementById("date").innerHTML = date;
+    document.getElementById("time").innerHTML = time;
+}
+
+
+setInterval(showDatetime, 1000); // calls showDateTime every 1 sec
