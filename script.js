e437820c44aeafe5d82c1cca3ef840186e7b5b81
Aditya <modaniaditya06@gmail.com>
2025-04-25 16:02:44 +0530

    navigation bar update

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function showDatetime() {
 
 
 setInterval(showDatetime, 1000); // calls showDateTime every 1 sec
+
+
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav ul li a');
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY; 
+    let offset =sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  })};
