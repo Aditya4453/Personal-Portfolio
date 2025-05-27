6002b20333437b2083831499d88588c6424f8fc1
Aditya <modaniaditya06@gmail.com>
2025-05-27 20:43:24 +0530

    update

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function showDatetime() {
-    const now = new Date();
-    const options = {
-        weekday: 'long',   // e.g., "Sunday"
-        year: 'numeric',   // e.g., "2025"
-        month: 'long',     // e.g., "April"
-        day: 'numeric'     // e.g., "20"
-      };
-      
-    const date = now.toLocaleDateString('en-IN', options);
-    const time = now.toLocaleTimeString();
+  const now = new Date();
+  const options = {
+    weekday: 'long',   // e.g., "Sunday"
+    year: 'numeric',   // e.g., "2025"
+    month: 'long',     // e.g., "April"
+    day: 'numeric'     // e.g., "20"
+  };
 
-    document.getElementById("date").innerHTML = date;
-    document.getElementById("time").innerHTML = time;
+  const date = now.toLocaleDateString('en-IN', options);
+  const time = now.toLocaleTimeString();
+
+  document.getElementById("date").innerHTML = date;
+  document.getElementById("time").innerHTML = time;
 }
 
 
 setInterval(showDatetime, 1000); // calls showDateTime every 1 sec
 
 
-
+// This is for when we scroll down to a page that name will be highlighted in navigation bar 
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav ul li a');
 window.onscroll = () => {
   sections.forEach(sec => {
-    let top = window.scrollY; 
-    let offset =sec.offsetTop;
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
     if (top >= offset && top < offset + height) {
@@ -32,5 +32,17 @@ window.onscroll = () => {
         links.classList.remove('active');
         document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
       });
-    };
-  })};
+    const an=document.querySelector(`#${id} h1`);
+    if (an){
+      gsap.from(an, {
+      color: "white",
+    }
+    );
+    }
+    
+    }
+  });
+};
+
+let about = document.querySelector('.undersign');
+
